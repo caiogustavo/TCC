af2799822b1287f7a32f3c7b4debb274df67c5ec
caiogustavo <caiogustavo.git@gmail.com>
2015-08-19 20:09:32 -0300

    Passos 4 a 6 após adicionar AES
    Foram seguidos os passos 4 a 6 do Lab 4 do Tutorial UG940 (v 2014.1),
    após ter sido adicionado o bloco AES (criado por mim) e o clk_wiz (que
    gera aes_clk a 32 MHz)

---
+++ vivado_projects/project_uBlaze-AES/project_uBlaze-AES.srcs/sources_1/ipshared/CGMA/AES_AXI_v1_0/9c12c494/sim_1/imports/new/TB_myip.vhd
@@ -0,0 +1,524 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 20.07.2015 23:29:21
+-- Design Name: 
+-- Module Name: TB_myip - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity TB_myip is
+--    Port ( );
+end TB_myip;
+
+architecture simulation of TB_myip is
+
+    signal bvalid, aresetn, awvalid, awready, wvalid, wready, bready, arvalid, arready, rvalid, rready : std_logic;
+    signal aes_clk, aclk: std_logic := '0';
+    signal awaddr : std_logic_vector(7-1 downto 0);
+    signal awprot : std_logic_vector(2 downto 0);
+    signal wdata : std_logic_vector(32-1 downto 0);
+    signal wstrb : std_logic_vector((32/8)-1 downto 0);
+    signal bresp : std_logic_vector(1 downto 0);
+    signal araddr : std_logic_vector(7-1 downto 0);
+    signal arprot : std_logic_vector(2 downto 0);
+    signal rdata : std_logic_vector(32-1 downto 0);
+    signal rresp : std_logic_vector(1 downto 0);
+    
+    signal bo_e, bo_d : std_logic_vector(127 downto 0);
+
+component myip_lite_v1_0
+	generic (
+	    C_S00_AXI_DATA_WIDTH	: integer	:= 32;
+		C_S00_AXI_ADDR_WIDTH	: integer	:= 7
+	);
+	port (
+		-- Users to add ports here
+        aes_clk : in std_logic;
+		-- User ports ends
+		s00_axi_aclk	: in std_logic;
+		s00_axi_aresetn	: in std_logic;
+		s00_axi_awaddr	: in std_logic_vector(C_S00_AXI_ADDR_WIDTH-1 downto 0);
+		s00_axi_awprot	: in std_logic_vector(2 downto 0);
+		s00_axi_awvalid	: in std_logic;
+		s00_axi_awready	: out std_logic;
+		s00_axi_wdata	: in std_logic_vector(C_S00_AXI_DATA_WIDTH-1 downto 0);
+		s00_axi_wstrb	: in std_logic_vector((C_S00_AXI_DATA_WIDTH/8)-1 downto 0);
+		s00_axi_wvalid	: in std_logic;
+		s00_axi_wready	: out std_logic;
+		s00_axi_bresp	: out std_logic_vector(1 downto 0);
+		s00_axi_bvalid	: out std_logic;
+		s00_axi_bready	: in std_logic;
+		s00_axi_araddr	: in std_logic_vector(C_S00_AXI_ADDR_WIDTH-1 downto 0);
+		s00_axi_arprot	: in std_logic_vector(2 downto 0);
+		s00_axi_arvalid	: in std_logic;
+		s00_axi_arready	: out std_logic;
+		s00_axi_rdata	: out std_logic_vector(C_S00_AXI_DATA_WIDTH-1 downto 0);
+		s00_axi_rresp	: out std_logic_vector(1 downto 0);
+		s00_axi_rvalid	: out std_logic;
+		s00_axi_rready	: in std_logic
+	);
+end component myip_lite_v1_0;
+
+begin
+
+
+myip_0: myip_lite_v1_0
+	generic map (
+	    C_S00_AXI_DATA_WIDTH => 32,
+		C_S00_AXI_ADDR_WIDTH => 7
+	)
+	port map (
+		-- Users to add ports here
+        aes_clk => aes_clk,
+		-- User ports ends
+		s00_axi_aclk => aclk,
+		s00_axi_aresetn => aresetn,
+		s00_axi_awaddr => awaddr,
+		s00_axi_awprot => awprot,
+		s00_axi_awvalid => awvalid,
+		s00_axi_awready => awready,
+		s00_axi_wdata => wdata,
+		s00_axi_wstrb => wstrb,
+		s00_axi_wvalid => wvalid,
+		s00_axi_wready => wready,
+		s00_axi_bresp => bresp,
+		s00_axi_bvalid => bvalid,
+		s00_axi_bready => bready,
+		s00_axi_araddr => araddr,
+		s00_axi_arprot => arprot,
+		s00_axi_arvalid => arvalid,
+		s00_axi_arready => arready,
+		s00_axi_rdata => rdata,
+		s00_axi_rresp => rresp,
+		s00_axi_rvalid => rvalid,
+		s00_axi_rready => rready
+	);
+--end myip_lite_v1_0;
+
+    --aresetn <= '1';
+
+clock_procSB: process
+begin
+    aes_clk <= aes_clk xor '1';
+    wait for 0.005 us; -- 100 MHz
+end process;
+clock_procAXI: process
+begin
+    aclk <= aclk xor '1';
+    wait for 0.5 us; -- 1 MHz
+end process;
+
+sim: process begin
+    
+    wait for 1 us;
+    
+    aresetn <= '0';
+
+    wait for 2 us;
+    
+    aresetn <= '1';
+    
+    wait for 2 us;
+    
+    -- Inicializa Registrador de status/config (reg16)
+    awaddr <= "1000000"; --reg16
+    awvalid <= '1';
+    wdata <= x"00000001"; -- demux deixa chave na saída
+    wvalid <= '1';
+    wstrb <= "1111";
+    bready <= '1';
+    wait for 4 us;
+    awaddr <= "0000000";
+    awvalid <= '0';
+    wdata <= x"00000000";
+    wvalid <= '0';
+    wstrb <= "0000";
+    bready <= '0';
+    
+    wait for 1 us;
+
+    -- Escreve a chave k0 parte 1
+    awaddr <= "0000000"; --reg0
+    awvalid <= '1';
+    wdata <= x"09cf4f3c";
+    wvalid <= '1';
+    wstrb <= "1111";
+    bready <= '1';
+    wait for 4 us;
+    awaddr <= "0000000";
+    awvalid <= '0';
+    wdata <= x"00000000";
+    wvalid <= '0';
+    wstrb <= "0000";
+    bready <= '0';
+    
+    wait for 1 us;
+    
+    -- Escreve a chave k0 parte 2
+    awaddr <= "0000100"; --reg1
+    awvalid <= '1';
+    wdata <= x"abf71588";
+    wvalid <= '1';
+    wstrb <= "1111";
+    bready <= '1';
+    wait for 4 us;
+    awaddr <= "0000000";
+    awvalid <= '0';
+    wdata <= x"00000000";
+    wvalid <= '0';
+    wstrb <= "0000";
+    bready <= '0';
+    
+    wait for 1 us;
+
+    -- Escreve a chave k0 parte 3
+    awaddr <= "0001000"; --reg2
+    awvalid <= '1';
+    wdata <= x"28aed2a6";
+    wvalid <= '1';
+    wstrb <= "1111";
+    bready <= '1';
+    wait for 4 us;
+    awaddr <= "0000000";
+    awvalid <= '0';
+    wdata <= x"00000000";
+    wvalid <= '0';
+    wstrb <= "0000";
+    bready <= '0';
+    
+    wait for 1 us;
+
+    -- Escreve a chave k0 parte 4
+    awaddr <= "0001100"; --reg3
+    awvalid <= '1';
+    wdata <= x"2b7e1516";
+    wvalid <= '1';
+    wstrb <= "1111";
+    bready <= '1';
+    wait for 4 us;
+    awaddr <= "0000000";
+    awvalid <= '0';
+    wdata <= x"00000000";
+    wvalid <= '0';
+    wstrb <= "0000";
+    bready <= '0';
+    
+    wait for 1 us;
+    
+    -- Inicializa KeyExpansion, setando en_k
+    awaddr <= "1000000"; --reg16
+    awvalid <= '1';
+    wdata <= x"00000009"; -- en_k = 1 e k = 1
+    wvalid <= '1';
+    wstrb <= "0001";
+    bready <= '1';
+    wait for 4 us;
+    awaddr <= "0000000";
+    awvalid <= '0';
+    wdata <= x"00000000";
+    wvalid <= '0';
+    wstrb <= "0000";
+    bready <= '0';
+    
+    wait for 1 us;
+    
+    -- Reseta Config para mudar demux
+    awaddr <= "1000000"; --reg16
+    awvalid <= '1';
+    wdata <= x"00000008"; -- en_k = 1 e k = 0
+    wvalid <= '1';
+    wstrb <= "0001";
+    bready <= '1';
+    wait for 4 us;
+    awaddr <= "0000000";
+    awvalid <= '0';
+    wdata <= x"00000000";
+    wvalid <= '0';
+    wstrb <= "0000";
+    bready <= '0';
+    
+    wait for 1 us;
+    
+    -- Escreve o bloco bi parte 1
+    awaddr <= "0000000"; --reg0
+    awvalid <= '1';
+    wdata <= x"e0370734";
+    wvalid <= '1';
+    wstrb <= "1111";
+    bready <= '1';
+    wait for 4 us;
+    awaddr <= "0000000";
+    awvalid <= '0';
+    wdata <= x"00000000";
+    wvalid <= '0';
+    wstrb <= "0000";
+    bready <= '0';
+    
+    wait for 1 us;
+    
+    -- Escreve o bloco bi parte 2
+    awaddr <= "0000100"; --reg1
+    awvalid <= '1';
+    wdata <= x"313198a2";
+    wvalid <= '1';
+    wstrb <= "1111";
+    bready <= '1';
+    wait for 4 us;
+    awaddr <= "0000000";
+    awvalid <= '0';
+    wdata <= x"00000000";
+    wvalid <= '0';
+    wstrb <= "0000";
+    bready <= '0';
+    
+    wait for 1 us;
+
+    -- Escreve o bloco bi parte 3
+    awaddr <= "0001000"; --reg2
+    awvalid <= '1';
+    wdata <= x"885a308d";
+    wvalid <= '1';
+    wstrb <= "1111";
+    bready <= '1';
+    wait for 4 us;
+    awaddr <= "0000000";
+    awvalid <= '0';
+    wdata <= x"00000000";
+    wvalid <= '0';
+    wstrb <= "0000";
+    bready <= '0';
+    
+    wait for 1 us;
+
+    -- Escreve o bloco bi parte 4
+    awaddr <= "0001100"; --reg3
+    awvalid <= '1';
+    wdata <= x"3243f6a8";
+    wvalid <= '1';
+    wstrb <= "1111";
+    bready <= '1';
+    wait for 4 us;
+    awaddr <= "0000000";
+    awvalid <= '0';
+    wdata <= x"00000000";
+    wvalid <= '0';
+    wstrb <= "0000";
+    bready <= '0';
+    
+    wait for 1 us;
+    
+    -- Inicializa Encryption
+    awaddr <= "1000000"; --reg16
+    awvalid <= '1';
+    wdata <= x"0000000A"; -- en_k = 1 e en_e = 1
+    wvalid <= '1';
+    wstrb <= "0001";
+    bready <= '1';
+    wait for 4 us;
+    awaddr <= "0000000";
+    awvalid <= '0';
+    wdata <= x"00000000";
+    wvalid <= '0';
+    wstrb <= "0000";
+    bready <= '0';
+    
+    wait for 1 us;
+    
+    -- Lê bloco bo_e
+    araddr <= "0010000"; -- reg4
+    arvalid <= '1';
+    wait for 2 us;
+    rready <= '1';
+    bo_e(1*32-1 downto 0*32) <= rdata;
+    wait for 2 us;
+    araddr <= "0010100"; -- reg5
+    arvalid <= '1';
+    wait for 2 us;
+    rready <= '1';
+    bo_e(2*32-1 downto 1*32) <= rdata;
+    wait for 2 us;
+    araddr <= "0011000"; -- reg6
+    arvalid <= '1';
+    wait for 2 us;
+    rready <= '1';
+    bo_e(3*32-1 downto 2*32) <= rdata;
+    wait for 2 us;
+    araddr <= "0011100"; -- reg7
+    arvalid <= '1';
+    wait for 2 us;
+    rready <= '1';
+    bo_e(4*32-1 downto 3*32) <= rdata;
+    
+    wait for 1 us;
+    
+    -- Escreve o bloco bi_d parte 1
+    awaddr <= "0100000"; --reg8
+    awvalid <= '1';
+    wdata <= bo_e(1*32-1 downto 0*32);
+    wvalid <= '1';
+    wstrb <= "1111";
+    bready <= '1';
+    wait for 4 us;
+    awaddr <= "0000000";
+    awvalid <= '0';
+    wdata <= x"00000000";
+    wvalid <= '0';
+    wstrb <= "0000";
+    bready <= '0';
+    
+    wait for 1 us;
+    
+    -- Escreve o bloco bi_d parte 2
+    awaddr <= "0100100"; --reg9
+    awvalid <= '1';
+    wdata <= bo_e(2*32-1 downto 1*32);
+    wvalid <= '1';
+    wstrb <= "1111";
+    bready <= '1';
+    wait for 4 us;
+    awaddr <= "0000000";
+    awvalid <= '0';
+    wdata <= x"00000000";
+    wvalid <= '0';
+    wstrb <= "0000";
+    bready <= '0';
+    
+    wait for 1 us;
+
+    -- Escreve o bloco bi_d parte 3
+    awaddr <= "0101000"; --reg10
+    awvalid <= '1';
+    wdata <= bo_e(3*32-1 downto 2*32);
+    wvalid <= '1';
+    wstrb <= "1111";
+    bready <= '1';
+    wait for 4 us;
+    awaddr <= "0000000";
+    awvalid <= '0';
+    wdata <= x"00000000";
+    wvalid <= '0';
+    wstrb <= "0000";
+    bready <= '0';
+    
+    wait for 1 us;
+
+    -- Escreve o bloco bi_d parte 4
+    awaddr <= "0101100"; --reg11
+    awvalid <= '1';
+    wdata <= bo_e(4*32-1 downto 3*32);
+    wvalid <= '1';
+    wstrb <= "1111";
+    bready <= '1';
+    wait for 4 us;
+    awaddr <= "0000000";
+    awvalid <= '0';
+    wdata <= x"00000000";
+    wvalid <= '0';
+    wstrb <= "0000";
+    bready <= '0';
+    
+    wait for 1 us;
+    
+    -- Inicializa Decryption
+    awaddr <= "1000000"; --reg16
+    awvalid <= '1';
+    wdata <= x"0000000C"; -- en_k = 1 e en_d = 1
+    wvalid <= '1';
+    wstrb <= "0001";
+    bready <= '1';
+    wait for 4 us;
+    awaddr <= "0000000";
+    awvalid <= '0';
+    wdata <= x"00000000";
+    wvalid <= '0';
+    wstrb <= "0000";
+    bready <= '0';
+    
+    wait for 1 us;
+
+    -- Lê bloco bo_d
+    araddr <= "0110000"; -- reg12
+    arvalid <= '1';
+    wait for 2 us;
+    rready <= '1';
+    bo_d(1*32-1 downto 0*32) <= rdata;
+    wait for 2 us;
+    araddr <= "0110100"; -- reg13
+    arvalid <= '1';
+    wait for 2 us;
+    rready <= '1';
+    bo_d(2*32-1 downto 1*32) <= rdata;
+    wait for 2 us;
+    araddr <= "0111000"; -- reg14
+    arvalid <= '1';
+    wait for 2 us;
+    rready <= '1';
+    bo_d(3*32-1 downto 2*32) <= rdata;
+    wait for 2 us;
+    araddr <= "0111100"; -- reg15
+    arvalid <= '1';
+    wait for 2 us;
+    rready <= '1';
+    bo_d(4*32-1 downto 3*32) <= rdata;
+    
+    wait for 1 us;
+   
+    -- Inicializa Registrador de status/config (reg16)
+--    awaddr <= "1000000"; --reg16
+--    awvalid <= '1';
+--    wdata <= x"00000000"; -- demux deixa chave na saída
+--    wvalid <= '1';
+--    wstrb <= "0001";
+--    bready <= '1';
+--    wait for 4 us;
+--    awaddr <= "0000000";
+--    awvalid <= '0';
+--    wdata <= x"00000000";
+--    wvalid <= '0';
+--    wstrb <= "0000";
+--    bready <= '0';
+    
+--    wait for 1 us;
+
+
+
+--    wait for 8 us;
+    
+--    araddr <= "1000000"; -- reg16
+--    arvalid <= '1';
+    
+--    wait for 2 us;
+    
+--    rready <= '1';
+    
+    wait for 5 us;
+    
+end process sim;
+
+
+end simulation;
---
+++ vivado_projects/project_uBlaze-AES/project_uBlaze-AES.srcs/sources_1/ipshared/CGMA/AES_AXI_v1_0/9c12c494/sources_1/imports/sources_1/imports/project_TCC_v1.srcs/sources_1/new/KeyExpansion.vhd
@@ -0,0 +1,191 @@
+----------------------------------------------------------------------------------
+-- Company: UnB
+-- Engineer: Caio Gustavo Mesquita Ângelo
+-- 
+-- Create Date: 08.02.2015 21:19:08
+-- Design Name: 
+-- Module Name: KeyExpansion - Behavioral
+-- Project Name: Módulo de Segurança
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+-- use work.new_types.all;
+use IEEE.std_logic_arith.all;
+use IEEE.std_logic_signed.all;
+use IEEE.std_logic_unsigned.all;
+
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+
+
+entity KeyExpansion is
+    Port (
+        clk: in std_logic; 
+        en: in std_logic;
+        Key: in std_logic_vector(127 downto 0);
+        Wsub: in std_logic_vector(31 downto 0);
+        WtoSub: out std_logic_vector(31 downto 0);
+        mode, rdy: out std_logic;
+        K0, K1, K2, K3, K4, K5, K6, K7, K8, K9, K10: out std_logic_vector(127 downto 0)
+    );
+end KeyExpansion;
+
+architecture Behavioral of KeyExpansion is
+
+--signal rdy_i: std_logic:='0';
+signal w0, w1, w2, w3, w4, w5, w6, w7: std_logic_vector(31 downto 0) := (others=>'0');
+signal RC: std_logic_vector(7 downto 0) := (0 => '1', others=>'0');
+
+begin
+ 
+---------------------------------------------------------------------------------------------------------
+-- Combinacional: gera a próxima chave (w4 & w5 & w6 & w7) a partir da chave anterior (w0 & w1 & w2 & w3)
+--      WtoSub: palavra enviada ao bloco SubBytes para que seus bytes sejam substituídos
+--      Wsub: palavra que retorana do bloco SubBytes
+---------------------------------------------------------------------------------------------------------    
+    WtoSub <= w3(23 downto 0) & w3(31 downto 24);
+    w4 <= (Wsub xor (RC & x"000000")) xor w0;
+    w5 <= w4 xor w1;
+    w6 <= w5 xor w2;
+    w7 <= w6 xor w3;
+
+---------------------------------------------------------------------------------------------------------
+-- seq: processo que cria uma máquina de estados que realiza as etapas para expansão da chave
+-- A entrada assíncrona en (enable) determina o início e reset da máquina
+--      en 0->1 : Começa a expansão (próximo estado: ESTADO_0)
+--      en = 0 : Volta ao estado inicial
+---------------------------------------------------------------------------------------------------------
+seq: process(clk,en)
+    type ESTADOS is (ESTADO_INICIAL, ESTADO_0, ESTADO_1, ESTADO_2, ESTADO_3, ESTADO_4, ESTADO_5, ESTADO_6, ESTADO_7,
+                     ESTADO_8, ESTADO_9, ESTADO_10, ESTADO_FINAL);
+    variable estado: ESTADOS := ESTADO_INICIAL;
+begin 
+-- Comportamento assíncrono do en (enable)
+if rising_edge(en) then
+    estado := ESTADO_0;
+end if;
+if en='0' then
+    estado := ESTADO_INICIAL;
+end if;
+
+-- Sequenciamento síncrono
+if rising_edge(clk) then
+    case estado is
+        when ESTADO_INICIAL =>
+            rdy <= '0';
+            mode <= '1';
+        when ESTADO_0 =>
+            mode <= '0';
+            RC <= x"01"; 
+            w0 <= Key(127 downto 96);
+            w1 <= Key(95 downto 64);
+            w2 <= Key(63 downto 32);
+            w3 <= Key(31 downto 0);
+            K0 <= Key;
+            estado := ESTADO_1;
+        when ESTADO_1 =>
+            K1 <= w4 & w5 & w6 & w7;
+            RC <= (RC(6 downto 0) & '0') xor ("000" & RC(7)&RC(7)&'0'&RC(7)&RC(7));
+            w0 <= w4;
+            w1 <= w5;
+            w2 <= w6;
+            w3 <= w7;
+            estado := ESTADO_2;
+        when ESTADO_2 =>
+            K2 <= w4 & w5 & w6 & w7;
+            RC <= (RC(6 downto 0) & '0') xor ("000" & RC(7)&RC(7)&'0'&RC(7)&RC(7));
+            w0 <= w4;
+            w1 <= w5;
+            w2 <= w6;
+            w3 <= w7;
+            estado := ESTADO_3;
+        when ESTADO_3 =>
+            K3 <= w4 & w5 & w6 & w7;
+            RC <= (RC(6 downto 0) & '0') xor ("000" & RC(7)&RC(7)&'0'&RC(7)&RC(7));
+            w0 <= w4;
+            w1 <= w5;
+            w2 <= w6;
+            w3 <= w7;
+            estado := ESTADO_4; 
+        when ESTADO_4 =>
+            K4 <= w4 & w5 & w6 & w7;
+            RC <= (RC(6 downto 0) & '0') xor ("000" & RC(7)&RC(7)&'0'&RC(7)&RC(7));
+            w0 <= w4;
+            w1 <= w5;
+            w2 <= w6;
+            w3 <= w7;
+            estado := ESTADO_5; 
+        when ESTADO_5 =>
+            K5 <= w4 & w5 & w6 & w7;
+            RC <= (RC(6 downto 0) & '0') xor ("000" & RC(7)&RC(7)&'0'&RC(7)&RC(7));
+            w0 <= w4;
+            w1 <= w5;
+            w2 <= w6;
+            w3 <= w7;
+            estado := ESTADO_6;       
+        when ESTADO_6 =>
+            K6 <= w4 & w5 & w6 & w7;
+            RC <= (RC(6 downto 0) & '0') xor ("000" & RC(7)&RC(7)&'0'&RC(7)&RC(7));
+            w0 <= w4;
+            w1 <= w5;
+            w2 <= w6;
+            w3 <= w7;
+            estado := ESTADO_7; 
+        when ESTADO_7 =>
+            K7 <= w4 & w5 & w6 & w7;
+            RC <= (RC(6 downto 0) & '0') xor ("000" & RC(7)&RC(7)&'0'&RC(7)&RC(7));
+            w0 <= w4;
+            w1 <= w5;
+            w2 <= w6;
+            w3 <= w7;
+            estado := ESTADO_8; 
+        when ESTADO_8 =>
+            K8 <= w4 & w5 & w6 & w7;
+            RC <= (RC(6 downto 0) & '0') xor ("000" & RC(7)&RC(7)&'0'&RC(7)&RC(7));
+            w0 <= w4;
+            w1 <= w5;
+            w2 <= w6;
+            w3 <= w7;
+            estado := ESTADO_9; 
+        when ESTADO_9 =>
+            K9 <= w4 & w5 & w6 & w7;
+            RC <= (RC(6 downto 0) & '0') xor ("000" & RC(7)&RC(7)&'0'&RC(7)&RC(7));
+            w0 <= w4;
+            w1 <= w5;
+            w2 <= w6;
+            w3 <= w7;
+            estado := ESTADO_10;         
+        when ESTADO_10 =>
+            K10 <= w4 & w5 & w6 & w7;
+            estado := ESTADO_FINAL;
+        when ESTADO_FINAL =>    
+            rdy <= '1';
+            mode <= '1';
+   end case; 
+end if;
+    
+end process;
+    
+
+end Behavioral;
